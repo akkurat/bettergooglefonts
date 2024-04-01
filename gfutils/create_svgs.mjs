1ed4b98b61403a35e8982fc6b6a82f571cbe672c
Moritz Vifian <moritz.vifian@gmx.ch>
2024-04-01 15:19:18 +0200

    Adding script for creating svg specimen

--- gfutils/create_svgs.mjs
+++ gfutils/create_svgs.mjs
@@ -1,31 +1,62 @@
 import { readFileSync, writeFileSync } from 'node:fs'
 import { join } from 'node:path';
+import { parseArgs } from "node:util";
 import TextToSVG from 'text-to-svg';
 import { MemoryDb } from 'minimongo'
 import { fileURLToPath } from 'node:url';
+import { time } from 'node:console';
 const __filename = fileURLToPath(import.meta.url);
 
 console.log(__filename)
 
 
-const db = new MemoryDb();
-db.addCollection('fonts')
+const args = parseArgs({
+    options: {
+        class: {
+            type: "boolean",
+            short: "c",
+        },
+        specimen: {
+            type: "boolean",
+            short: "s"
+        }
+    },
+    allowPositionals: true
+});
 
-const fontParamsSans = JSON.parse(readFileSync('../bettergooglefontsng/src/assets/classification_questions.json'))
-const metas = JSON.parse(readFileSync('../bettergooglefontsng/src/assets/fontmeta.json'))
-const classificationEntries = JSON.parse(readFileSync('../bettergooglefontsng/src/assets/classification.json'))
-const classification = new Map(classificationEntries)
+console.log(args)
+if (args.values.class) {
+    createSvgs()
+} else if (args.values.specimen) {
+    if (args.positionals.length >= 2) {
+        createSpecimen(args.positionals)
+    } else {
+        throw new Error(`At least 2 positional arguments are required for specimen. Only ${args.positionals.length} were given`)
+    }
 
-for (const meta of metas) {
-    meta['classification'] = classification.get(meta.meta.name)
 }
 
-db.collections['fonts'].upsert(metas,
-    (docs) => { console.log(docs.length), createSvgs(db) },
-    (err) => { console.log(err) }
-)
 
-async function createSvgs(db) {
+async function initDb() {
+
+    const db = new MemoryDb();
+    db.addCollection('fonts')
+
+    const classificationEntries = JSON.parse(readFileSync('../bettergooglefontsng/src/assets/classification.json'))
+    const classification = new Map(classificationEntries)
+    const metas = JSON.parse(readFileSync('../bettergooglefontsng/src/assets/fontmeta.json'))
+
+    for (const meta of metas) {
+        meta['classification'] = classification.get(meta.meta.name)
+    }
+    const docs = await db.collections['fonts'].upsert(metas);
+    console.log(docs.length)
+    return db
+}
+
+async function createSvgs() {
+    const db = await initDb();
+    const fontParamsSans = JSON.parse(readFileSync('../bettergooglefontsng/src/assets/classification_questions.json'))
     for (const [k, v] of Object.entries(fontParamsSans)) {
 
         console.log(`${k}`)
@@ -87,35 +118,60 @@ async function createSvgs(db) {
             x += letter.textToSVG.getWidth(letter.letter, options)
             paths += letter.textToSVG.getPath(letter.letter, options)
         }
-
         writeFileSync(join('prevsvgs', `${k}.svg`), svgHeader(paths))
     }
 }
-function svgHeaderSquare(path) {
-    return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-            <svg
-               xmlns="http://www.w3.org/2000/svg"
-               xmlns:svg="http://www.w3.org/2000/svg"
-               width="100"
-               height="100"
-               version="1.1"
-               id="svg1"
-               viewBox="0 0 100 100"
-               >
-               ${path}
-            </svg>`;
+
+async function createSpecimen(args) {
+    const db = await initDb();
+    const [specimen, ...fontNames] = args
+
+    const prevsvgname = Date.now() + '.svg'
+    console.log(prevsvgname)
+
+    const letters = []
+    for (const fontName of fontNames) {
+        const firstMatch = await db.fonts.findOne({ 'meta.name': fontName })
+
+        if (!firstMatch) {
+            console.warn(`Font with name "${fontName}" was not found`)
+            continue
+        }
+
+        console.log(firstMatch)
+
+        const textToSVG = TextToSVG.loadSync(join('..', firstMatch.dir, firstMatch.meta.fonts[0].filename))
+        // const textToSVG = TextToSVG.loadSync()
+        letters.push({ textToSVG, letter: specimen })
+
+    }
+
+    let y = 10, width = 0; // starty
+    let paths = ""
+    for (const letter of letters) {
+        const options = { x: 10, y, anchor: 'left top', fontSize: 50 }
+        const metrics = letter.textToSVG.getMetrics(letter.letter, options)
+        y += metrics.height
+        width = Math.max(width, metrics.width)
+        paths += letter.textToSVG.getPath(letter.letter, options)
+    }
+    writeFileSync(join('out', prevsvgname), svgHeader(paths, width, y + 10))
 }
 
-function svgHeader(path) {
+function svgHeaderSquare(path) {
+    return svgHeader(path, 100, 100)
+}
+
+function svgHeader(path, width = 200, height = 100) {
     return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
             <svg
                xmlns="http://www.w3.org/2000/svg"
                xmlns:svg="http://www.w3.org/2000/svg"
-               width="200"
-               height="100"
+               width="${width}"
+               height="${height}"
                version="1.1"
                id="svg1"
-               viewBox="0 0 200 100"
+               viewBox="0 0 ${width} ${height}"
                >
                ${path}
             </svg>`;
